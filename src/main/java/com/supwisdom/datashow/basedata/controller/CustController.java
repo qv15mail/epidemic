758e906bf5395a1e44a34499bfffe3bc4ea44f1d
jiwei.han <jiwei.han@supwisdom.com>
2020-03-27 10:42:49 +0800

    记录查询修改排序

--- src/main/java/com/supwisdom/datashow/basedata/controller/CustController.java
+++ src/main/java/com/supwisdom/datashow/basedata/controller/CustController.java
@@ -237,7 +237,7 @@ public class CustController {
                     succCnt++;
                     tmpi++;
                 }catch (Exception ex){
-                    log.error(ex.getMessage());
+                    log.error(ex.getMessage()+",stuempno:"+stuempno);
                     errCnt++;
                 }
             }
